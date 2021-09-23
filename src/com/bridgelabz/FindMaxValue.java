09bbd66cc9aa9accdfef267d3fda9ff18a1bacb8
koresachin7 <koresachin7@gmail.com>
2021-09-23 22:38:20 +0530

    Added : Using Generic Class to Find  The Maximum Value of Integer Float String

--- src/com/bridgelabz/FindMaxValue.java
+++ src/com/bridgelabz/FindMaxValue.java
@@ -1,6 +1,15 @@
 package com.bridgelabz;
 
-public class FindMaxValue {
+public class FindMaxValue<T extends Comparable<T>> {
+
+    T a1, a2, a3;
+
+    public FindMaxValue(T a1, T a2, T a3) {
+        this.a1 = a1;
+        this.a2 = a2;
+        this.a3 = a3;
+    }
+
     /**
      * Generic method to compare any type of data and find maximum
      * a1 : First value to compare
