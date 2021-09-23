ea607d8980f64c0ee9ae82a4debae6779130b606
koresachin7 <koresachin7@gmail.com>
2021-09-23 21:50:56 +0530

    Added : Give Three Intergers And Find The Maximum

---
+++ src/com/bridgelabz/FindMaxValue.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+
+public class FindMaxValue {
+    /**
+     * Method used to compare three integer values and get the maximum value.
+     * a1 : First value to compare
+     * a2 : Second value to compare
+     * a3 : Third value to compare
+     * return max : Maximum of three integers
+     */
+    public static Integer maxOfInteger(Integer a1, Integer a2, Integer a3) {
+        Integer max = a1;
+        if (a2.compareTo(max) > 0)
+            max = a2;
+        if (a3.compareTo(max) > 0)
+            max = a3;
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the program to find maximum value ");
+        Integer a1 = 30, a2 = 40, a3 = 90;
+        System.out.println("The Maximum between three integers is : " + maxOfInteger(a1, a2, a3));
+    }
+}
+
