425bf18ad8c1d1f930f626269d7cd7d7be5148fb
koresachin7 <koresachin7@gmail.com>
2021-09-23 22:55:14 +0530

    Added : Compare More Than Three Parameters and use sorting

--- src/com/bridgelabz/FindMaxValue.java
+++ src/com/bridgelabz/FindMaxValue.java
@@ -1,38 +1,33 @@
 package com.bridgelabz;
 
+import java.util.Arrays;
+
 public class FindMaxValue<T extends Comparable<T>> {
 
-    T a1, a2, a3;
+    T[] elements;
 
-    public FindMaxValue(T a1, T a2, T a3) {
-        this.a1 = a1;
-        this.a2 = a2;
-        this.a3 = a3;
+    public FindMaxValue(T[] elements) {
+        this.elements = elements;
     }
 
     /**
-     * Generic method to compare any type of data and find maximum
-     * a1 : First value to compare
-     * a2 : Second value to compare
-     * a3 : Third value to compare
+     * Generic method to store values in array and return maximum value
      * return max : Maximum of three values
      */
-    public static <T extends Comparable<T>> T maxOfValues(T a1, T a2, T a3) {
-        T max = a1;
-        if (a2.compareTo(max) > 0)
-            max = a2;
-        if (a3.compareTo(max) > 0)
-            max = a3;
+    public static <T extends Comparable<T>> T maxOfValues(T[] elements) {
+        Arrays.sort(elements);
+        int lenght = elements.length;
+        T max = elements[lenght - 1];
         return max;
     }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the program to find maximum value ");
-        Integer a1 = 30, a2 = 40, a3 = 90;
-        System.out.println("The Maximum between three integers is : " + maxOfValues(a1, a2, a3));
-        Float f1 = 3.5f, f2 = 9.4f, f3 = 7.1f;
-        System.out.println("The Maximum between three float is : " + maxOfValues(f1, f2, f3));
-        String s1 = "abc", s2 = "pqr", s3 = "xyz";
-        System.out.println("The Maximum between three String is : " + maxOfValues(s1, s2, s3));
+        System.out.println("Welcome to the program to find maximum value using generics");
+        Integer[] intMax = { 10, 20, 30, 40, 21, 16, 99,8 };
+        System.out.println("The Maximum between three integers is : " + maxOfValues(intMax));
+        Float[] floatMax = { 7.5f, 45.3f, 78.2f, 87.3f, 54.6f };
+        System.out.println("The Maximum between three float is : " + maxOfValues(floatMax));
+        String[] stringMax = { "abc", "hgl", "bpl", "ipl", "bbc", "ddn", "psl", "xyz" };
+        System.out.println("The Maximum between three String is : " + maxOfValues(stringMax));
     }
 }
-
